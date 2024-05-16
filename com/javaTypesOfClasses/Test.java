c19fd3e23f3e1bab23bac87e656849392ae7a5bf
VYANKATESH ROKADE <91716803+vyankiii@users.noreply.github.com>
2024-05-16 12:59:56 +0530

    Update Test.java

--- com/javaTypesOfClasses/Test.java
+++ com/javaTypesOfClasses/Test.java
@@ -4,6 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		NormalGeneralConcrete obj = new NormalGeneralConcrete();
+		//Abstract abs = new Abstract(); //Cannot instantiate the type Abstract
+
+		// Final  f = new Final(); //Cannot instantiate the type Final
+
+		// FunctioanlInterface FI = new FunctionalInterface();//Cannot instantiate the type FunctioanlInterface
+		//InterfaceClass IC = new InterfaceClass();// Cannot instantiate the type InterfaceClass
 
 	}
 
